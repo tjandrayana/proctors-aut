7dcaca1d9b8ff86ca13730eafde78c2f80512c06
tjandrayana.setiawan <tjandrayana.setiawan@BA-00025807.local>
2023-05-25 18:32:42 +0700

    modify commands

--- proctor-aut.go
+++ proctor-aut.go
@@ -25,20 +25,47 @@ type Instances struct {
 }
 
 type DeleteVM struct {
-	ServiceName string `json:"service_name"`
-	Environment string `json:"environment"`
-	Team        string `json:"team"`
-	Notes       string `json:"notes"`
+	ServiceName   string `json:"service_name"`
+	Environment   string `json:"environment"`
+	Team          string `json:"team"`
+	ProjectName   string `json:"project"`
+	Notes         string `json:"notes"`
+	ExecutionDate string `json:"execution_date"`
+}
+
+type AddingGCloudLabel struct {
+	ResourceName string `json:"service_name"`
+	ResourceType string `json:"resource_type"`
+	Environment  string `json:"environment"`
+	ProjectID    string `json:"project_id"`
+	TeamName     string `json:"team_name"`
+	Zone         string `json:"zone"`
+	AppName      string `json:"app_name"`
+
+	Notes         string `json:"notes"`
+	ExecutionDate string `json:"execution_date"`
+}
+
+type ResizeVM struct {
+	ServiceName   string `json:"service_name"`
+	Environment   string `json:"environment"`
+	Team          string `json:"team"`
+	ProjectName   string `json:"project"`
+	UpdatedVMType string `json:"updated_vm_type"`
+	Notes         string `json:"notes"`
+	ExecutionDate string `json:"execution_date"`
 }
 
 const (
-	DescribeInstanceCommand string = "describe-instance"
-	DeleteVMCommand         string = "delete-vm"
+	DescribeInstanceCommand  string = "describe-instance"
+	DeleteVMCommand          string = "delete-vm"
+	AddingGCloudLabelCommand string = "add-gcloud-resource-labels"
+	ResizeVMCommand          string = "resize-vm"
 )
 
 func main() {
 
-	var filename = "delete-vm-consumer-risk"
+	var filename = "delete_request_25_05_2023"
 	command := DeleteVMCommand
 
 	RunCommands(command, filename)
@@ -56,6 +83,10 @@ func RunCommands(command, filename string) {
 		RunDescribeInstanceCommand(filename, records, totalWorker)
 	case DeleteVMCommand:
 		RunDeleteVMCommand(filename, records, totalWorker)
+	case AddingGCloudLabelCommand:
+		RunAddingGCloudLabelCommand(filename, records, totalWorker)
+	case ResizeVMCommand:
+		RunResizeVMCommand(filename, records, totalWorker)
 	default:
 		fmt.Printf("Unknown Command %s\n", command)
 	}
@@ -124,6 +155,86 @@ func RunDescribeInstanceCommand(filename string, records [][]string, totalWorker
 
 }
 
+func RunAddingGCloudLabelCommand(filename string, records [][]string, totalWorker int) {
+
+	// Start Worker Pool.
+	wp := workerpool.NewWorkerPool(totalWorker)
+	wp.Run()
+
+	var addingGCloudLabel []AddingGCloudLabel
+
+	totalTask := len(records)
+	resultC := make(chan AddingGCloudLabel, totalTask)
+
+	for i, d := range records {
+		d := d
+		i := i
+
+		wp.AddTask(
+
+			func() {
+
+				resourceName := d[0]
+				env := d[1]
+				teamName := d[2]
+				resourceType := d[3]
+				projectID := d[4]
+				zone := d[5]
+				appName := d[6]
+
+				request := AddingGCloudLabel{
+					ResourceName: resourceName,
+					ResourceType: resourceType,
+					Environment:  env,
+					ProjectID:    projectID,
+					TeamName:     teamName,
+					Zone:         zone,
+					AppName:      appName,
+				}
+
+				fmt.Printf("---- Processing Task : %d--- %s -> %s ----- \n", i, request.ResourceName, env)
+
+				if i > 0 {
+					if env == "production" || env == "integration" {
+						args := GetAddingGCloudLabelCommand(request)
+						msg := ExecuteCommand(args)
+
+						resultC <- EvaluateAddingGCloudLabelOutput(msg, request)
+					} else {
+						resultC <- AddingGCloudLabel{
+							ResourceName: request.ResourceName,
+							ResourceType: request.ResourceType,
+							Environment:  request.Environment,
+							ProjectID:    request.ProjectID,
+							TeamName:     request.TeamName,
+							Zone:         request.Zone,
+							AppName:      request.AppName,
+
+							Notes:         "No Action",
+							ExecutionDate: fmt.Sprintf("%d-%d-%d", time.Now().Year(), time.Now().Month(), time.Now().Day()),
+						}
+					}
+				} else {
+					resultC <- AddingGCloudLabel{}
+				}
+
+			},
+		)
+
+	}
+
+	for i := 0; i < totalTask; i++ {
+		res := <-resultC
+		if i == 0 {
+			continue
+		}
+		addingGCloudLabel = append(addingGCloudLabel, res)
+	}
+
+	CreateCSV(addingGCloudLabel, filename, AddingGCloudLabelCommand)
+
+}
+
 func RunDeleteVMCommand(filename string, records [][]string, totalWorker int) {
 
 	// Start Worker Pool.
@@ -146,22 +257,27 @@ func RunDeleteVMCommand(filename string, records [][]string, totalWorker int) {
 				insName := d[0]
 				env := d[1]
 				team := d[2]
+				projectName := d[3]
 
 				fmt.Printf("---- Processing Task : %d--- %s -> %s ----- \n", i, insName, env)
 
+				request := DeleteVM{
+					ServiceName:   insName,
+					Environment:   env,
+					Team:          team,
+					ProjectName:   projectName,
+					ExecutionDate: fmt.Sprintf("%d-%d-%d", time.Now().Year(), time.Now().Month(), time.Now().Day()),
+				}
+
 				if i > 0 {
 					if env == "production" || env == "integration" {
-						args := GetDeleteVMCommand(insName, env)
+						args := request.GetDeleteVMCommand()
 						msg := ExecuteCommand(args)
 
-						resultC <- EvaluateDeleteVMOutput(msg, insName, env, team)
+						resultC <- request.EvaluateDeleteVMOutput(msg)
 					} else {
-						resultC <- DeleteVM{
-							ServiceName: insName,
-							Environment: env,
-							Team:        team,
-							Notes:       "No Action",
-						}
+						request.Notes = "No Action"
+						resultC <- request
 					}
 				} else {
 					resultC <- DeleteVM{}
@@ -196,27 +312,33 @@ func GetDescribeInstanceCommand(instanceName, environment string) []string {
 
 }
 
-func GetDeleteVMCommand(vmName, environment string) []string {
-
-	/*
-		proctor execute delete-vm
-		VM_NAME=g-fraud-device-tigergraph-db-c-02
-		PROJECT_NAME=rabbit-hole-integration-007 PURGE=true DELETE_DISKS=all
-
-	*/
-
-	projectName := "rabbit-hole-integration-007"
-	if environment == "production" {
-		projectName = "infrastructure-904"
-	}
+func (d DeleteVM) GetDeleteVMCommand() []string {
 
 	args := []string{
 		"execute",
 		"delete-vm",
-		fmt.Sprintf("VM_NAME=%s", vmName),
-		fmt.Sprintf("PROJECT_NAME=%s", projectName),
+		fmt.Sprintf("VM_NAME=%s", d.ServiceName),
+		fmt.Sprintf("PROJECT_NAME=%s", d.ProjectName),
 		fmt.Sprintf("PURGE=%v", true),
 		fmt.Sprintf("DELETE_DISK=%s", "all"),
+	}
+
+	return args
+
+}
+
+func GetAddingGCloudLabelCommand(request AddingGCloudLabel) []string {
+
+	args := []string{
+		"execute",
+		"add-gcloud-resource-labels",
+		fmt.Sprintf("RESOURCES_NAME=%s", request.ResourceName),
+		fmt.Sprintf("RESOURCE_TYPE=%s", request.ResourceType),
+		fmt.Sprintf("PROJECT_ID=%s", request.ProjectID),
+		fmt.Sprintf("TEAM_NAME=%s", request.TeamName),
+		fmt.Sprintf("ZONE=%s", request.Zone),
+		fmt.Sprintf("APP_NAME=%s", request.AppName),
+		fmt.Sprintf("APP=%s", request.AppName),
 	}
 
 	return args
@@ -233,7 +355,7 @@ func ExecuteCommand(args []string) string {
 	return string(msg)
 }
 
-func EvaluateDeleteVMOutput(msg, insName, environment, team string) DeleteVM {
+func (d DeleteVM) EvaluateDeleteVMOutput(msg string) DeleteVM {
 
 	notes := "No Action"
 
@@ -252,11 +374,43 @@ func EvaluateDeleteVMOutput(msg, insName, environment, team string) DeleteVM {
 
 	}
 
-	return DeleteVM{
-		ServiceName: insName,
-		Environment: environment,
-		Team:        team,
-		Notes:       notes,
+	d.Notes = notes
+
+	return d
+}
+
+func EvaluateAddingGCloudLabelOutput(msg string, request AddingGCloudLabel) AddingGCloudLabel {
+
+	notes := "No Action"
+
+	m := strings.ToLower(msg)
+	resps := strings.Split(m, "\n")
+
+	fmt.Printf("[EvaluateAddingGCloudLabelOutput] -> Messages %s\n\n\n", m)
+
+	for _, d := range resps {
+		if d == "" {
+			continue
+		}
+		fmt.Printf("[EvaluateAddingGCloudLabelOutput] -> %s\n", d)
+		note := strings.Contains(d, "added following labels on instance")
+		if note {
+			notes = d
+		}
+
+	}
+
+	return AddingGCloudLabel{
+		ResourceName: request.ResourceName,
+		ResourceType: request.ResourceType,
+		Environment:  request.Environment,
+		ProjectID:    request.ProjectID,
+		TeamName:     request.TeamName,
+		Zone:         request.Zone,
+		AppName:      request.AppName,
+
+		Notes:         notes,
+		ExecutionDate: fmt.Sprintf("%d-%d-%d", time.Now().Year(), time.Now().Month(), time.Now().Day()),
 	}
 
 }
@@ -359,7 +513,9 @@ func CreateCSV(data interface{}, filename, command string) {
 			"ServiceName",
 			"Environment",
 			"Team",
+			"Project",
 			"Notes",
+			"Execution Date",
 		})
 
 		ins := data.([]DeleteVM)
@@ -369,10 +525,70 @@ func CreateCSV(data interface{}, filename, command string) {
 				record.ServiceName,
 				record.Environment,
 				record.Team,
+				record.ProjectName,
 				record.Notes,
+				record.ExecutionDate,
 			}
 			rows = append(rows, row)
 		}
+
+	case AddingGCloudLabelCommand:
+		rows = append(rows, []string{
+			"ResourceName",
+			"Environment",
+			"Team",
+			"Zone",
+			"ResourceType",
+			"ProjectID",
+			"AppName",
+			"Notes",
+			"Execution Date",
+		})
+
+		ins := data.([]AddingGCloudLabel)
+
+		for _, record := range ins {
+			row := []string{
+				record.ResourceName,
+				record.Environment,
+				record.TeamName,
+				record.Zone,
+				record.ResourceType,
+				record.ProjectID,
+				record.AppName,
+				record.Notes,
+				record.ExecutionDate,
+			}
+			rows = append(rows, row)
+		}
+
+	case ResizeVMCommand:
+
+		rows = append(rows, []string{
+			"ServiceName",
+			"Environment",
+			"Team",
+			"Project",
+			"UpdatedVMType",
+			"Notes",
+			"Execution Date",
+		})
+
+		ins := data.([]ResizeVM)
+
+		for _, record := range ins {
+			row := []string{
+				record.ServiceName,
+				record.Environment,
+				record.Team,
+				record.ProjectName,
+				record.UpdatedVMType,
+				record.Notes,
+				record.ExecutionDate,
+			}
+			rows = append(rows, row)
+		}
+
 	default:
 		return
 	}
@@ -403,4 +619,111 @@ func readCsvFile(filePath string) [][]string {
 	}
 
 	return records
+}
+
+func RunResizeVMCommand(filename string, records [][]string, totalWorker int) {
+
+	// Start Worker Pool.
+	wp := workerpool.NewWorkerPool(totalWorker)
+	wp.Run()
+
+	var resizeVM []ResizeVM
+
+	totalTask := len(records)
+	resultC := make(chan ResizeVM, totalTask)
+
+	for i, d := range records {
+		d := d
+		i := i
+
+		wp.AddTask(
+
+			func() {
+
+				insName := d[0]
+				env := d[1]
+				team := d[2]
+				projectName := d[3]
+				updatedVMType := d[4]
+
+				fmt.Printf("---- Processing Task : %d--- %s -> %s ----- \n", i, insName, env)
+
+				request := ResizeVM{
+					ServiceName:   insName,
+					Environment:   env,
+					Team:          team,
+					ProjectName:   projectName,
+					UpdatedVMType: updatedVMType,
+					ExecutionDate: fmt.Sprintf("%d-%d-%d", time.Now().Year(), time.Now().Month(), time.Now().Day()),
+				}
+
+				if i > 0 {
+					if env == "production" || env == "integration" {
+						args := request.GetResizeVMCommand()
+						msg := ExecuteCommand(args)
+
+						resultC <- request.EvaluateResizeVMOutput(msg)
+					} else {
+						request.Notes = "No Action"
+						resultC <- request
+					}
+				} else {
+					resultC <- ResizeVM{}
+				}
+
+			},
+		)
+
+	}
+
+	for i := 0; i < totalTask; i++ {
+		res := <-resultC
+		if i == 0 {
+			continue
+		}
+		resizeVM = append(resizeVM, res)
+	}
+
+	CreateCSV(resizeVM, filename, DeleteVMCommand)
+
+}
+
+func (d ResizeVM) GetResizeVMCommand() []string {
+
+	args := []string{
+		"execute",
+		"resize-vm",
+		fmt.Sprintf("VM_NAME=%s", d.ServiceName),
+		fmt.Sprintf("PROJECT_ID=%s", d.ProjectName),
+		fmt.Sprintf("UPDATED_VM_TYPE=%v", d.UpdatedVMType),
+	}
+
+	return args
+
+}
+
+func (r ResizeVM) EvaluateResizeVMOutput(msg string) ResizeVM {
+
+	notes := "No Action"
+
+	m := strings.ToLower(msg)
+	resps := strings.Split(m, "\n")
+
+	for _, d := range resps {
+		if d == "" {
+			continue
+		}
+
+		info := strings.Contains(d, "[info] Action:")
+		if info {
+			notes = d
+		} else {
+			notes = fmt.Sprintf("Update %s to be %s in the project %s success", r.ServiceName, r.UpdatedVMType, r.Environment)
+		}
+
+	}
+
+	r.Notes = notes
+
+	return r
 }
